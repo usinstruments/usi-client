dccf552b9ef208d8ce586cedef9971816d4cc96f
Sagar Patil <sagarreddypatil@gmail.com>
2024-05-21 00:48:35 -0400

    initial commit

---
+++ esbuild.main.config.ts
@@ -0,0 +1,25 @@
+import type { BuildOptions } from 'esbuild'
+import * as path from 'node:path'
+
+const main: BuildOptions = {
+  platform: 'node',
+  entryPoints: [path.resolve('src/main/main.ts')],
+  bundle: true,
+  format: 'esm',
+  target: 'node20.11.1', // electron version target
+}
+
+// const preload: BuildOptions = {
+//   platform: 'node',
+//   entryPoints: [path.resolve('src/main/preload.ts')],
+//   bundle: true,
+//   format: 'esm',
+//   target: 'node20.11.1', // electron version target
+//   // it is important to use .mjs extension for preload script because of how electron load preload script
+//   outExtension: {
+//     '.js': '.mjs',
+//   }
+// }
+
+// export default [main, preload]
+export default [main]
---
+++ src/main/main.ts
@@ -0,0 +1,66 @@
+import * as path from  'path'
+import * as url from 'url'
+import { app, BrowserWindow } from 'electron'
+import { isDev } from 'electron-util/main'
+import { is } from 'electron-util'
+
+let win: BrowserWindow | null = null
+
+const dirname = path.dirname(new URL(import.meta.url).pathname)
+
+async function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 820,
+    minHeight: 600,
+    minWidth: 650,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    show: false,
+  })
+
+  if (isDev) {
+    win.loadURL('http://localhost:9080')
+  } else {
+    win.loadURL(
+      url.format({
+        pathname: path.join(dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true,
+      }),
+    )
+  }
+
+  win.on('closed', () => {
+    win = null
+  })
+
+  win.webContents.on('devtools-opened', () => {
+    win!.focus()
+  })
+
+  win.on('ready-to-show', () => {
+    win!.show()
+    win!.focus()
+
+    if (isDev) {
+      win!.webContents.openDevTools({ mode: 'bottom' })
+    }
+  })
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (!is.macos) {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null && app.isReady()) {
+    createWindow()
+  }
+})
