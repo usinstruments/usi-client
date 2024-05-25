adddaa269a682bf67b1bf0cfeddaa5f44f4d1a4e
Sagar Patil <sagarreddypatil@gmail.com>
2024-05-25 05:11:36 -0400

    tomfoolery

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,6 +2,16 @@ import React from "react";
 import { useUser } from "./Login.tsx";
 import { MdLogout } from "react-icons/md";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import { IoCodeWorkingOutline, IoCodeWorkingSharp, IoFolderSharp, IoGitBranch, IoGitBranchSharp, IoLogOutSharp, IoReorderThreeSharp, IoShapesSharp } from "react-icons/io5";
+import { GrProjects } from "react-icons/gr";
+import { FaProjectDiagram } from "react-icons/fa";
+
+enum Order {
+    Normal,
+    Flipped
+}
+
+let order = Order.Flipped;
 
 export default function App() {
   const { user, logout } = useUser();
@@ -14,30 +24,62 @@ export default function App() {
 
   return (
     <div className="flex flex-row h-svh">
-      <div className="flex-1">
+      <div className={`flex-1 ${order === Order.Normal ? "order-2" : "order-1"}`}>
+        {order === Order.Flipped ? (
         <PanelGroup direction="horizontal">
           <Panel order={1}>
-            <div className="flex flex-col h-full">
-              <div className="flex flex-row border-b border-gray-200 dark:border-gray-800 h-10">
-                {tabs.map((tab) => (
-                  <div className="flex flex-row items-center px-4 py-2 gap-2 select-none border-x border-gray-200 dark:border-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900">
-                    <span>{tab}</span>
-                    {/* <button className="w-5 h-5 flex flex-row items-center justify-center border-none">x</button> */}
-                  </div>
-                ))}
-              </div>
-              <div className="flex-1 bg-gray-100 dark:bg-gray-900"></div>
-            </div>
+            {TabsView(tabs)}
           </Panel>
-          <PanelResizeHandle className="w-[1px] bg-gray-200 dark:bg-gray-800 relative active:bg-black hover:bg-gray-400 dark:active:bg-white after:absolute after:w-[4px] after:h-full after:-left-[1px] before:absolute before:h-full before:w-[16px] before:-left-[6px] before:z-50" />
-          <Panel order={2}>Panel B</Panel>
-        </PanelGroup>
+          <PanelResizeHandle className="panel-resize-handle" />
+          <Panel order={2} minSizePixels={160}>
+            {SidebarView()}
+          </Panel>
+        </PanelGroup>) : (
+        <PanelGroup direction="horizontal">
+          <Panel order={1} minSizePixels={160}>
+            {SidebarView()}
+          </Panel>
+          <PanelResizeHandle className="panel-resize-handle" />
+          <Panel order={2}>
+            {TabsView(tabs)}
+          </Panel>
+        </PanelGroup>)}
       </div>
-      <div className="right-buttons">
+      <div className={`right-buttons ${order === Order.Normal ? "order-1" : "order-2"}`}>
+        <button title="Projects">
+            <IoFolderSharp />
+        </button>
+        <button title="Projects">
+            <IoCodeWorkingSharp />
+        </button>
+        <button title="Template Repos">
+            <IoGitBranchSharp />
+        </button>
         <button className="mt-auto overflow-hidden" title="Logout" onClick={logout}>
-            <MdLogout className="ms-1 min-w-8 min-h-8" />
+            <IoLogOutSharp className="ms-1" />
         </button>
       </div>
     </div>
   );
 }
+
+function SidebarView() {
+  return <div className="min-w-40">
+    Panel B
+  </div>;
+}
+
+function TabsView(tabs: string[]) {
+    return <div className="flex flex-col h-full">
+        <div className="tabs">
+            {tabs.map((tab) => (
+                <div className="tab" key={tab}>
+                    <span>{tab}</span>
+                    {/* <button className="w-5 h-5 flex flex-row items-center justify-center border-none">x</button> */}
+                </div>
+            ))}
+        </div>
+        <div className="flex-1 bg-gray-100 dark:bg-gray-900"></div>
+    </div>;
+}
+
